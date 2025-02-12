55a058b3c1fb1db243a35064c700c8450e8dda2e
musta <musta@LAPTOP-S7VDJD25>
2025-02-12 15:23:04 +0300

    Add project files.

---
+++ Eticaret.Core/Entities/AppUser.cs
@@ -0,0 +1,28 @@
+﻿namespace Eticaret.Core.Entities
+{
+    public class AppUser : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string Password { get; set; }
+
+        public string? UserName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public DateTime CreatDate { get; set;}
+
+        public Guid? UserGuid { get; set; } = Guid.NewGuid();
+
+    }
+}
