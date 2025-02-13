e6b958b29b829a1082747de39f6cac820dd81e3a
mustafacil45 <mustafacil986@gmail.com>
2025-02-13 16:21:26 +0300

    commit first

---
+++ Eticaret.Core/Entities/Contact.cs
@@ -0,0 +1,19 @@
+﻿namespace Eticaret.Core.Entities
+{
+    public class Contact : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
