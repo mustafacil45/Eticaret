e70e2211f8a79dc254dbece1e6aead28f914d0b4
mustafacil45 <mustafacil986@gmail.com>
2025-04-19 03:24:29 +0300

    iletişim formlarını listeleme

---
+++ Eticaret.WebUI/Areas/Admin/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eticaret.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactsController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public ContactsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Contacts);
+        }
+    }
+}
