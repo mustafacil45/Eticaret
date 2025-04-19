fa5329512d5221512d0a20fa2d78ad071502632c
mustafacil45 <mustafacil986@gmail.com>
2025-04-20 02:46:04 +0300

    Kampanya ve Duyurular modülü

---
+++ Eticaret.WebUI/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Eticaret.Core.Entities;
+using Eticaret.Data;
+
+namespace Eticaret.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NewsController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public NewsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/News
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.News.ToListAsync());
+        }
+
+        // GET: Admin/News/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // GET: Admin/News/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Image,IsActive")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(news);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(news);
+        }
+
+        // GET: Admin/News/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News.FindAsync(id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+            return View(news);
+        }
+
+        // POST: Admin/News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Image,IsActive")] News news)
+        {
+            if (id != news.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(news);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NewsExists(news.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(news);
+        }
+
+        // GET: Admin/News/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // POST: Admin/News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var news = await _context.News.FindAsync(id);
+            if (news != null)
+            {
+                _context.News.Remove(news);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NewsExists(int id)
+        {
+            return _context.News.Any(e => e.Id == id);
+        }
+    }
+}
