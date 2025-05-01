a3f8b21bca9168bccae594deff7f770351eb6b83
mustafacil45 <mustafacil986@gmail.com>
2025-05-01 13:49:32 +0300

    Kategoriler Modülü

---
+++ Eticaret.WebUI/ViewComponents/Categories.cs
@@ -0,0 +1,21 @@
+﻿using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eticaret.WebUI.ViewComponents
+{
+    public class Categories : ViewComponent
+    {
+        private readonly DataBaseContext _context;
+
+        public Categories(DataBaseContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
