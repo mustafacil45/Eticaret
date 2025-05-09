2660dbb22df2e10eda5789b91898b1ce8011d659
mustafacil45 <mustafacil986@gmail.com>
2025-05-09 18:47:02 +0300

    siteye arama motoru ekleme

--- Eticaret.WebUI/Controllers/ProductsController.cs
+++ Eticaret.WebUI/Controllers/ProductsController.cs
@@ -14,9 +14,9 @@ namespace Eticaret.WebUI.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string q = "")
         {
-            var dataBaseContext = _context.Products.Where(p => p.IsActive).Include(p => p.Brand).Include(p => p.Category);
+            var dataBaseContext = _context.Products.Where(p => p.IsActive && p.Name.Contains(q) || p.Name.Contains(q)).Include(p => p.Brand).Include(p => p.Category);
             return View(await dataBaseContext.ToListAsync());
         }
 
