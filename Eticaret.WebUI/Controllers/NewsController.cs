47d472ea3e06c4618d361568cfb373a9c24fdedc
mustafacil45 <mustafacil986@gmail.com>
2025-05-09 16:28:50 +0300

    Kampanya detay ekranı

---
+++ Eticaret.WebUI/Controllers/NewsController.cs
@@ -0,0 +1,37 @@
+﻿using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eticaret.WebUI.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public NewsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.News.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+    }
+}
