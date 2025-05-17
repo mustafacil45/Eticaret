e5f9c1ede3b3c145369993a41a9aa9c95fd8cc2b
mustafacil45 <mustafacil986@gmail.com>
2025-05-18 00:06:32 +0300

    üye kayıt işlemleri

--- Eticaret.WebUI/Controllers/AccountController.cs
+++ Eticaret.WebUI/Controllers/AccountController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eticaret.Core.Entities;
+using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Eticaret.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DataBaseContext _context;
+
+        public AccountController(DataBaseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,9 +20,27 @@ namespace Eticaret.WebUI.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SingIn(AppUser appUser)
+        {
+            return View();
+        }
         public IActionResult SingUp()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SingUpAsync(AppUser appUser)
+        {
+            appUser.IsAdmin = false;
+            appUser.IsActive = true;
+            if (ModelState.IsValid)
+            {
+                await _context.AddAsync(appUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appUser);
         }
     }
 }
