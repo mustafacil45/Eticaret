e9fbcade441c79d38d1c437b416c1ae55fdb08a9
mustafacil45 <mustafacil986@gmail.com>
2025-05-17 23:31:33 +0300

    üye sayfalarını oluşturma ve üye giriş formu

---
+++ Eticaret.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eticaret.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SingIn()
+        {
+            return View();
+        }
+        public IActionResult SingUp()
+        {
+            return View();
+        }
+    }
+}
