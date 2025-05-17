539037f5206fb20935d235a654f302293c6c43b6
mustafacil45 <mustafacil986@gmail.com>
2025-05-18 00:23:06 +0300

    üye giriş modeli

---
+++ Eticaret.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eticaret.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string? ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
