d4b1e41a1804806860ccfcfe14f01c36b88e73c3
mustafacil45 <mustafacil986@gmail.com>
2025-05-21 20:38:41 +0300

    session servisini özelleştirme

--- Eticaret.WebUI/Program.cs
+++ Eticaret.WebUI/Program.cs
@@ -13,7 +13,15 @@ namespace Eticaret.WebUI
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddSession();
+            builder.Services.AddSession(options =>
+            {
+                options.Cookie.Name = ".Eticaret.Session";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                options.IdleTimeout = TimeSpan.FromDays(1);
+                options.IOTimeout = TimeSpan.FromMinutes(10);
+
+            });
 
             builder.Services.AddDbContext<DataBaseContext>();
 
